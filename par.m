634cb6c7b8a5113490d5653e545ad6480ea55c6d
wangxueli4542 <96574474+wangxueli4542@users.noreply.github.com>
2024-02-01 19:27:43 +0800

    hh

--- par.m
+++ par.m
@@ -13,10 +13,5 @@ c_foam = rho_foam*v_foam*cp_foam*1e-3;%发泡层总热容
 rho_al= 2719 % 铝密度
 cp_al = 871 % 铝的比热容
 v_al_in = 0.68% 柜内铝型材散热器体积L
-v_al_out=1.6%柜外铝型材散热器体积L
-c_al_in=rho_al*v_al_in*cp_al*1e-3;% 柜内铝型材散热器总热容
-c_al_out=rho_al*v_al_out*cp_al*1e-3;% 柜外铝型材散热器总热容
-K=4*K_guiti % 柜内空气与发泡层和柜外空气与发泡层的热导，假设为通过发泡层导热的的一半
-K_al_in =40;% 柜内空气与柜内散热器的热导
-k_al_out =40%柜外空气与柜内散热器的热导
+
 
